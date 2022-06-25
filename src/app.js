f6a64b3f0706e35611337d99fe77ac531b5af269
Александр Пушкин <aleksandrpushkin@MacBook-Air-Aleksandr.local>
2022-06-25 20:32:02 +0300

    Common Fixes

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ import './styles.css';
 import { ContextMenu } from './menu';
 import { ClicksModule } from './modules/clicks.module';
 import { StageModule } from './modules/stage.module';
+import { BackgroundModule } from './modules/background.module';
 
 const contextMenu = new ContextMenu('.menu');
 contextMenu.add(new ClicksModule('clicks', 'Анализ кликов'));
 contextMenu.add(new StageModule('stage', 'Время поиграть'));
+contextMenu.add(new BackgroundModule('background', 'Изменить цвет'));
--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,15 @@
-import {Module} from '../core/module'
+import { Module } from '../core/module';
+import { random } from '../../src/utils';
 
 export class BackgroundModule extends Module {
 
+    trigger() {
+        const letters = "0123456789ABCDEF";
+        let color = "#";
+
+        for (let i = 0; i < 6; i++) {
+            color += letters[random(0.5, 15)];
+        }
+        document.body.style.background = color;
+    }
 }
