973e844479f4d4ba0e44af3aa56e8364bf3804c2
Александр Пушкин <aleksandrpushkin@MacBook-Air-Aleksandr.local>
2022-06-25 16:00:23 +0300

    Stage Module completed

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ import './styles.css';
 import { ContextMenu } from './menu';
 import { PlugOne } from './modules/plugOne';
 import { PlugTwo } from './modules/plugTwo';
+import { StageModule } from './modules/stage.module';
 
 const contextMenu = new ContextMenu('.menu');
 const plugOne = new PlugOne('plugOne', 'Название модуля 1');
 const plugTwo = new PlugTwo('plugTwo', 'Название модуля 2');
 contextMenu.add(plugOne);
 contextMenu.add(plugTwo);
+contextMenu.add(new StageModule('stage', 'Время поиграть'));
