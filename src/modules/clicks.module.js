389ba9714d051ffde1b1e65efa45222fd251242d
fl0wka <vadimus-garbage@mail.ru>
2022-06-25 17:53:13 +0200

    added countdown

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,5 +1,4 @@
 import {Module} from '../core/module'
-import {ContextMenu} from '../core/menu'
 
 export class ClicksModule extends Module {
     constructor(type, text) {
@@ -7,7 +6,12 @@ export class ClicksModule extends Module {
     }
 
     trigger() {
+        const viewTimer = document.createElement('div');
+        const messageTimer = document.createElement('h1');
+        const timerInHtml = document.createElement('p');
         const square = document.createElement('div');
+        viewTimer.style.textAlign = 'center';
+        timerInHtml.style.fontSize = '40px';
         square.style.position = 'fixed';
 		square.style.zIndex = '20';
 		square.style.width = '200px';
@@ -24,23 +28,46 @@ export class ClicksModule extends Module {
 		square.style.transition = 'opacity 0.7s ease 0s';
 
         document.body.append(square);
+        viewTimer.append(messageTimer);
+        viewTimer.append(timerInHtml);
+        document.body.append(viewTimer);
+
+        let timer;
+        let startTimerCounter = 3;
         let count = 0;
+
+        countdown();
 
         function clickCounter() {
             square.style.fontSize = '80px';
             square.textContent = count += 1;
         }
 
-        document.body.addEventListener('click', clickCounter);
-        
-        setTimeout(() => {
-            document.body.removeEventListener('click', clickCounter);
-            square.style.fontSize = '18px';
-            square.textContent = `Вы кликнули ${count} раз`;
-        }, 3000);
+        function countdown() {
+            timerInHtml.innerHTML = startTimerCounter;
+            startTimerCounter -= 1;
+            if (startTimerCounter < 0) {
+                clearTimeout(timer);
+                messageTimer.textContent = 'Жми на мышь!';
+                timerInHtml.textContent = '';
+
+                document.body.addEventListener('click', clickCounter);
+
+                setTimeout(() => {
+                    document.body.removeEventListener('click', clickCounter);
+                    messageTimer.textContent = '';
+                    square.style.fontSize = '18px';
+                    square.textContent = `Вы кликнули ${count} раз`;
+                }, 3000);
+            } else {
+                messageTimer.textContent = 'Приготовьтесь кликать!';
+                timer = setTimeout(countdown, 1000);
+            }
+        }
+
 
         setTimeout(() => {
 			square.remove();
-		}, 5000);
+		}, 8000);
     }
 }
