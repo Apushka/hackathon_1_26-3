c6d396e72132fe24898324cc3dd22d55617a48a3
Helga116 <102963963+Helga116@users.noreply.github.com>
2022-06-25 16:05:40 +0300

    add background module

--- src/app.js
+++ src/app.js
@@ -1,2 +1,11 @@
 import './styles.css'
 
+import {BackgroundModule} from './modules/background.module';
+
+console.log('__work__');
+
+const backgroundChange = new BackgroundModule();
+backgroundChange.trigger();
+console.log(backgroundChange);
+// createFunc();
+
--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,26 @@
-import {Module} from '../core/module'
+import {Module} from '../core/module';
+import {random} from '../../src/utils';
 
 export class BackgroundModule extends Module {
 
+  constructor() {
+    super('change background', 'Now!')
+    this.bodyContainer = document.querySelector('body');
+    this.bodyContainer.className = 'body';
+    console.log(this.bodyContainer);
+  }
+
+  getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[random(0.5, 15)];
+    }
+    return color;
+  }
+
+  trigger() {
+    this.bodyContainer.style.background = this.getRandomColor();
+    return this.bodyContainer;
+  }
 }
