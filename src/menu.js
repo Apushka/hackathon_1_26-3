57b62f1d336cad78a1a1ea28eae21fbd2dd2cb92
Alex Pushkin <apushkaa@gmail.com>
2022-06-26 20:26:04 +0300

    test push mouse

--- src/menu.js
+++ src/menu.js
@@ -12,7 +12,10 @@ export class ContextMenu extends Menu {
 
 	open(event) {
 		this.el.classList.add('open');
-		getMenuPosition(this.el, event);
+		this.el.style.left = event.clientX + 'px';
+		this.el.style.top = event.clientY + 'px';
+
+		// getMenuPosition(this.el, event);
 	};
 
 	close() {
--- src/utils.js
+++ src/utils.js
@@ -68,7 +68,7 @@ export function unblockContext() {
 }
 
 function handleContext(e) {
-	e.preventDefault();
+	// e.preventDefault();
 	e.stopPropagation();
 }
 
