fb913376f7c6515a2f61d04b9e46095ca450b690
Alex Pushkin <apushkaa@gmail.com>
2022-06-26 09:57:32 +0300

    coin module added

--- src/app.js
+++ src/app.js
@@ -4,9 +4,11 @@ import { ClicksModule } from './modules/clicks.module';
 import { StageModule } from './modules/stage.module';
 import { BackgroundModule } from './modules/background.module';
 import { DoubleGame } from './modules/double.module';
+import { CoinModule } from './modules/coin.module';
 
 const contextMenu = new ContextMenu('.menu');
 contextMenu.add(new ClicksModule('clicks', 'Анализ кликов'));
 contextMenu.add(new StageModule('stage', 'Время поиграть'));
 contextMenu.add(new BackgroundModule('background', 'Изменить цвет'));
 contextMenu.add(new DoubleGame('doublegame', 'Игра "Найди пару"'));
+contextMenu.add(CoinModule('coin', 'Орёл или Решка'));
