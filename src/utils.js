beeecc190dd2b7cb1ad289065c4bd106c4710654
Alex Pushkin <apushkaa@gmail.com>
2022-06-26 09:31:17 +0300

    common fixes

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,14 @@
 export function random(min, max) {
 	return Math.round(min - 0.5 + Math.random() * (max - min + 1))
 };
+
+export function getRandomColor(min, max) {
+	const letters = "0123456789ABCDEF";
+	let color = "#";
+
+	for (let i = 0; i < 6; i++) {
+		color += letters[random(min, max)];
+	}
+	
+	return color;
+}
